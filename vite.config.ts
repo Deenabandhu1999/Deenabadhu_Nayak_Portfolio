af51e113fb7abbb6381148695f81c4efd7bbaaab
Deenabandhu Nayak <81607184+Deenabandhu1999@users.noreply.github.com>
2025-11-19 10:16:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Deenabadhu_Nayak_Portfolio/',  // 👈 ADD THIS LINE
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
